2eec102403ceba1e6031866d0d52c721f07b6571
OMI786I <sohrawardy1998@gmail.com>
2024-10-12 13:44:35 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./layout/Root.jsx";
+import Home from "./home/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
