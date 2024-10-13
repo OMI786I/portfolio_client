81e2422e2d08c7c6a47c67f93a7c62f1451ac424
OMI786I <sohrawardy1998@gmail.com>
2024-10-13 13:39:05 +0600

    added sticky navbar and completed footer

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../shared/Navbar";
+import Footer from "../shared/Footer";
 
 const Root = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,16 +1,68 @@
 import React from "react";
+import { Link } from "react-scroll";
 
 const Footer = () => {
   return (
-    <footer className="footer bg-neutral text-neutral-content p-10">
+    <footer className="footer bg-success text-neutral-content p-10">
       <nav>
-        <h6 className="footer-title">Services</h6>
+        <Link
+          to="servicesSection"
+          smooth={true}
+          duration={500}
+          className="cursor-pointer"
+        >
+          <button className=" font-bold p-2 duration-200 hover:border-b-green-600 ">
+            Services
+          </button>
+        </Link>
       </nav>
       <nav>
-        <h6 className="footer-title">Company</h6>
+        <Link
+          to="recentProjectSection"
+          smooth={true}
+          duration={500}
+          className="cursor-pointer"
+        >
+          <button className="font-bold p-2 duration-200 hover:border-b-green-600 ">
+            Recent Projects
+          </button>
+        </Link>
       </nav>
       <nav>
-        <h6 className="footer-title">Legal</h6>
+        <Link
+          to="missionVisionSection"
+          smooth={true}
+          duration={500}
+          className="cursor-pointer"
+        >
+          <button className="font-bold p-2 duration-200 hover:border-b-green-600 ">
+            Mission & Vision
+          </button>
+        </Link>
+      </nav>
+      <nav>
+        <Link
+          to="blogsSection"
+          smooth={true}
+          duration={500}
+          className="cursor-pointer"
+        >
+          <button className="font-bold p-2 duration-200 hover:border-b-green-600 ">
+            Blogs
+          </button>
+        </Link>
+      </nav>
+      <nav>
+        <Link
+          to="contactSection"
+          smooth={true}
+          duration={500}
+          className="cursor-pointer"
+        >
+          <button className="font-bold p-2 duration-200 hover:border-b-green-600 ">
+            Contact
+          </button>
+        </Link>
       </nav>
     </footer>
   );
